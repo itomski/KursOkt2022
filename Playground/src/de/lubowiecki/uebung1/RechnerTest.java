adb1e61ecee7b59ca86f90519d026088f57650c4
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-10-10 08:40:41 +0200

    Übung 1

--- Playground/src/de/lubowiecki/uebung1/RechnerTest.java
+++ Playground/src/de/lubowiecki/uebung1/RechnerTest.java
@@ -3,32 +3,93 @@ package de.lubowiecki.uebung1;
 import java.util.Scanner;
 
 public class RechnerTest {
-
+	
 	public static void main(String[] args) {
+		//ohneProtokoll();
+		mitProtokoll();
+		
+	}
+	
+
+	private static void ohneProtokoll() {
 		
 		final Scanner scanner = new Scanner(System.in);
 		
+		double a;
+		double b;
+		
 		while(true) {
-			System.out.println("Welche Operation?");
 			
+			System.out.print("\nOperation: ");
 			String op = scanner.next();
 			
+			if(op.equalsIgnoreCase("exit")) break;
+			
+			System.out.print("\na: ");
+			a = scanner.nextDouble();
+			
+			System.out.print("\nb: ");
+			b = scanner.nextDouble();
+			
 			if(op.equalsIgnoreCase("add")) {
-				System.out.println("Zahlen abfragen und addieren");
+				System.out.println(Rechner.add(a, b));
 			}
 			else if(op.equalsIgnoreCase("multi")) {
-				System.out.println("Zahlen abfragen und multiplizieren");
+				System.out.println(Rechner.multi(a, b));
 			}
 			else if(op.equalsIgnoreCase("sub")) {
-				System.out.println("Zahlen abfragen und sub");
+				System.out.println(Rechner.sub(a, b));
 			}
 			else if(op.equalsIgnoreCase("div")) {
-				System.out.println("Zahlen abfragen und div");
+				System.out.println(Rechner.div(a, b));
 			}
-			else if(op.equalsIgnoreCase("exit")) {
-				break;
+			
+		}
+		
+		System.out.println("\nENDE");
+	}
+	
+	private static void mitProtokoll() {
+		
+		ProtokollRechner rechner = new ProtokollRechner();
+		
+		final Scanner scanner = new Scanner(System.in);
+		
+		double a;
+		double b;
+		
+		while(true) {
+			
+			System.out.print("\nOperation: ");
+			String op = scanner.next();
+			
+			if(op.equalsIgnoreCase("exit")) break;
+			
+			else if(op.equalsIgnoreCase("protokoll")) {
+				System.out.println(rechner.getProtokoll());
+				continue;
+			}
+			
+			System.out.print("\na: ");
+			a = scanner.nextDouble();
+			
+			System.out.print("\nb: ");
+			b = scanner.nextDouble();
+			
+			if(op.equalsIgnoreCase("add")) {
+				System.out.println(rechner.add(a, b));
+			}
+			else if(op.equalsIgnoreCase("multi")) {
+				System.out.println(rechner.multi(a, b));
+			}
+			else if(op.equalsIgnoreCase("sub")) {
+				System.out.println(rechner.sub(a, b));
+			}
+			else if(op.equalsIgnoreCase("div")) {
+				System.out.println(rechner.div(a, b));
 			}
 		}
+		
+		System.out.println("\nENDE");
 	}
-
 }
