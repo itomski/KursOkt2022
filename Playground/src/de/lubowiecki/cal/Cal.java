0b8d3ba9a546939fc4100db3f7a145da26c37343
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-11-02 13:43:16 +0100

    Update

---
+++ Playground/src/de/lubowiecki/cal/Cal.java
@@ -0,0 +1,36 @@
+package de.lubowiecki.cal;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Cal {
+	
+	private Map<LocalDate, String> termine = new TreeMap<>();
+
+	public Map<LocalDate, String> getTermine() {
+		return termine;
+	}
+
+	public void addTermin(LocalDate datum, String titel) {
+		this.termine.put(datum, titel);
+	}
+	
+	// Refactoring
+	private String getTermineAsString() {
+		
+		StringBuilder output = new StringBuilder(termine.size() * 30);
+		
+		for(LocalDate k : termine.keySet()) {
+			output.append(k).append(": ").append(termine.get(k));
+			output.append("\n");
+		}
+		
+		return output.toString();
+	}
+	
+	@Override
+	public String toString() {
+		return getTermineAsString();
+	}
+}
