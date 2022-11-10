378803785c89745970e905b947bb6b54fc938bf1
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-11-10 13:57:39 +0100

    Update

---
+++ client/src/main/java/de/lubowiecki/client/Product.java
@@ -0,0 +1,76 @@
+package de.lubowiecki.client;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+// Model
+public class Product implements Serializable {
+	
+	private String name;
+	private String description;
+	private int amount;
+	private double price;
+	private LocalDate createdAt; // YYYY-MM-dd
+
+	public Product() {
+		createdAt = LocalDate.now();
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product [name=")
+			.append(name)
+			.append(", description=")
+			.append(description)
+			.append(", amount=")
+			.append(amount)
+			.append(", price=")
+			.append(price)
+			.append(", createdAt=")
+			.append(createdAt)
+			.append("]");
+		
+		return builder.toString();
+	}	
+}
---
+++ client/src/main/java/de/lubowiecki/client/ProductController.java
@@ -0,0 +1,71 @@
+package de.lubowiecki.client;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ProductController implements Initializable { // Initializable bietet die Möglichkeit Tätigkeiten beim Starten der GUI auszuführen
+	
+	@FXML
+	private TextField name;
+	
+	@FXML
+	private TextArea description;
+	
+	@FXML
+	private TextField amount;
+	
+	@FXML
+	private TextField price;
+	
+	@FXML
+	private TableView<Product> tblProducts;
+	
+	private ProductManagement management = new ProductManagement();
+
+    @FXML
+    private void save() throws IOException {
+    	Product product = new Product();
+    	product.setName(name.getText());
+    	product.setDescription(description.getText());
+    	product.setAmount(Integer.parseInt(amount.getText()));
+    	product.setPrice(Double.parseDouble(price.getText()));
+    	management.add(product);
+    	clearForm();
+    	show();
+    }
+    
+    private void show() {
+//    	String row = "%s, %s, %s, %d, %.2f \n";
+//    	
+//    	StringBuilder sb = new StringBuilder();
+//    	for(Product p : management.getAll()) {
+//    		sb.append(String.format(row, p.getName(), p.getDescription(), p.getCreatedAt(), p.getAmount(), p.getPrice()));
+//    	}
+//    	output.setText(sb.toString());
+    	
+    	List<Product> productList = management.getAll();
+    	tblProducts.setItems(FXCollections.observableList(productList));
+    }
+    
+    private void clearForm() {
+    	name.clear();
+    	description.clear();
+    	amount.clear();
+    	price.clear();
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		show();
+	}
+}
