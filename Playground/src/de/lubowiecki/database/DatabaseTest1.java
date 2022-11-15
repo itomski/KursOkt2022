c977751dea90f6fdd02940939293ac745634b373
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-11-15 14:31:55 +0100

    Update

--- Playground/src/de/lubowiecki/database/DatabaseTest1.java
+++ Playground/src/de/lubowiecki/database/DatabaseTest1.java
@@ -5,13 +5,36 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class DatabaseTest1 {
 
-	private static final String URL = "jdbc:sqlite:data.db";
-	
-	
 	public static void main(String[] args) {
+		
+		UserRepository repo = new UserRepository();
+		try {
+			
+			// Alle abfragen
+//			List<User> users = repo.find();
+//			for(User u : users) {
+//				System.out.println(u.getFirstname() + " " + u.getLastname());
+//			}
+			
+			// Einen abfragen
+			User u = repo.findById(3);
+			System.out.println(u.getFirstname() + " " + u.getLastname());
+			
+		}
+		catch (UnsupportedOperationException e) {
+			System.out.println("Funktion ist noch nicht eingebaut.");
+		}
+		catch (Exception e) {
+			System.out.println("Probleme: " + e.getMessage());
+		}
+		
+		
+		System.exit(0);
+		
 		
 		// Connection con = DriverManager.getConnection(URL)
 		// Lädt den passenden Treiber und baut die Verbindung zur DB auf
@@ -22,8 +45,8 @@ public class DatabaseTest1 {
 		// stmt.executeQuery("SELECT * FROM users");
 		// Führt die Anfrage an die DB aus
 		
-		try(Connection con = DriverManager.getConnection(URL);
-				Statement stmt = con.createStatement()) {
+//		try(Connection con = DriverManager.getConnection(URL);
+//				Statement stmt = con.createStatement()) {
 			
 			// CRUD
 			// Create: INSERT
@@ -50,26 +73,26 @@ public class DatabaseTest1 {
 //			}
 			
 			// Update
-			int num = stmt.executeUpdate("UPDATE users SET firstname = 'Hans' WHERE id = 1");
-			if(num > 0) {
-				System.out.println("Wurde geändert...");
-			}
-			else {
-				System.out.println("Wurde NICHT geändert...");
-			}
-			
-			
-			// Select
-			ResultSet results = stmt.executeQuery("SELECT * FROM users"); // executeQuery: Für Abfragen
-			
-			final String ROW = "%10s | %10s | %10s \n";
-			
-			while(results.next()) {
-				System.out.printf(ROW, results.getString("id"), results.getString("firstname"), results.getString("lastname"));
-			}
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
+//			int num = stmt.executeUpdate("UPDATE users SET firstname = 'Hans' WHERE id = 1");
+//			if(num > 0) {
+//				System.out.println("Wurde geändert...");
+//			}
+//			else {
+//				System.out.println("Wurde NICHT geändert...");
+//			}
+//			
+//			
+//			// Select
+//			ResultSet results = stmt.executeQuery("SELECT * FROM users"); // executeQuery: Für Abfragen
+//			
+//			final String ROW = "%10s | %10s | %10s \n";
+//			
+//			while(results.next()) {
+//				System.out.printf(ROW, results.getString("id"), results.getString("firstname"), results.getString("lastname"));
+//			}
+//		} 
+//		catch (SQLException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
