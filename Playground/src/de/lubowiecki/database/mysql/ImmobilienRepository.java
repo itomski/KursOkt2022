3c8c750b2da10dc5a1f62e30b7466e5e182be1ee
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-12-05 10:02:23 +0100

    Updates

--- Playground/src/de/lubowiecki/database/mysql/ImmobilienRepository.java
+++ Playground/src/de/lubowiecki/database/mysql/ImmobilienRepository.java
@@ -1,32 +1,116 @@
 package de.lubowiecki.database.mysql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class ImmobilienRepository {
+	
+	private static final String TABLE = "immobilien";
+	
 
-	// TODO: Von direker ausgabe auf eine List mit Immobilien-Objekten umstellen
-	public void showAll() {
+	public void showAll() throws SQLException {
+		
+		for(Immobilie i : getAll()) {
+			System.out.println(i.getId());
+			System.out.println(i.getTitel());
+			System.out.println(i.getBeschreibung());
+			System.out.println(i.getGroesse());
+			System.out.println(i.getPreis());
+			System.out.println();
+		}
+	}
+	
+	public boolean insert(Immobilie immo) throws SQLException {
+		
+		final String SQL = "INSERT INTO " + TABLE + " (id, titel, beschreibung, groesse, preis)"
+						 + " VALUES(NULL, ?, ?, ?, ?)";
+		
+		try(Connection dbh = DatabaseUtils.createConnection(); 
+				PreparedStatement stmt = dbh.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS)) {
+			
+			stmt.setString(1, immo.getTitel());
+			stmt.setString(2, immo.getBeschreibung());
+			stmt.setInt(3, immo.getGroesse());
+			stmt.setDouble(4, immo.getPreis());
+			stmt.execute();
+			
+			ResultSet keys = stmt.getGeneratedKeys();
+			if(keys.next()) {
+				immo.setId(keys.getLong(1));
+				return true;
+			}
+			
+			return false;
+		}
+	}
+	
+	public List<Immobilie> getAll() throws SQLException {
+		
 		try(Connection dbh = DatabaseUtils.createConnection(); Statement stmt = dbh.createStatement()) {
 					
-			final String SQL = "SELECT * FROM immobilien";
+			final String SQL = "SELECT * FROM " + TABLE;
 			
 			ResultSet results = stmt.executeQuery(SQL);
 			
-			// Auf Liste mit Immonilien umstellen
+			List<Immobilie> immobilien = new ArrayList<>();
+			
 			while (results.next()) {
-				System.out.println(results.getLong("id"));
-				System.out.println(results.getString("titel"));
-				System.out.println(results.getString("beschreibung"));
-				System.out.println(results.getInt("groesse"));
-				System.out.println(results.getDouble("preis"));
-				System.out.println();
+				immobilien.add(create(results));
 			}
-		} 
-		catch (SQLException e) {
-			System.out.println("Problem beim Verbinden mit der Datenbank: " + e.getMessage());
+			
+			return immobilien;
 		}
+	}
+	
+	public Optional<Immobilie> getOne(long id) throws SQLException {
+		
+		try(Connection dbh = DatabaseUtils.createConnection(); Statement stmt = dbh.createStatement()) {
+					
+			final String SQL = "SELECT * FROM " + TABLE + " WHERE id = " + id;
+			
+			ResultSet results = stmt.executeQuery(SQL);
+			
+			if(results.next()) {
+				return Optional.of(create(results));
+			}
+			
+			return Optional.empty();
+		}
+	}
+	
+//	public Immobilie getOne(long id) throws SQLException {
+//		
+//		try(Connection dbh = DatabaseUtils.createConnection(); Statement stmt = dbh.createStatement()) {
+//					
+//			final String SQL = "SELECT * FROM " + TABLE + " WHERE id = " + id;
+//			
+//			ResultSet results = stmt.executeQuery(SQL);
+//			
+//			if(results.next()) {
+//				return create(results);
+//			}
+//			
+//			return null;
+//		}
+//	}
+	
+	
+	public Immobilie create(ResultSet result) throws SQLException {
+		
+		Immobilie i = new Immobilie();
+		i.setId(result.getLong("id"));
+		i.setTitel(result.getString("titel"));
+		i.setBeschreibung(result.getString("beschreibung"));
+		i.setGroesse(result.getInt("groesse"));
+		i.setPreis(result.getDouble("preis"));
+			
+		return i;
+		
 	}
 }
--- Playground/src/de/lubowiecki/database/mysql/MysqlConTestBetter.java
+++ Playground/src/de/lubowiecki/database/mysql/MysqlConTestBetter.java
@@ -1,11 +1,43 @@
 package de.lubowiecki.database.mysql;
 
+import java.sql.SQLException;
+import java.util.Optional;
+
 public class MysqlConTestBetter {
 
 	public static void main(String[] args) {
 		
 		ImmobilienRepository repo = new ImmobilienRepository();
-		repo.showAll();
+		
+		try {
+			repo.showAll();
+			
+//			Immobilie i = repo.getOne(10);
+//			System.out.println(i.getTitel());
+			
+			//Optional<Immobilie> opt = repo.getOne(10);
+//			if(opt.isPresent()) 
+//				System.out.println(opt.get().getId());
+			
+			//opt.ifPresent(i -> System.out.println(i.getTitel()));
+			
+			//opt.ifPresentOrElse(i -> System.out.println(i.getTitel()), () -> System.out.println("Nicht da"));
+			
+			
+//			Immobilie immo = new Immobilie();
+//			immo.setTitel("Nette Wohnung");
+//			immo.setBeschreibung("Nett aber ziemlich runtergerockt...");
+//			immo.setGroesse(100);
+//			immo.setPreis(200_000);
+//			
+//			if(repo.insert(immo)) {
+//				System.out.println(immo.getId());
+//			}
+			
+		} 
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		
 		// new ImmobilienRepository().showAll();
 		
