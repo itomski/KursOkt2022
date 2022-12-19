c993061b828e3a7d7e12e52c0d80fbfb636539aa
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-12-19 10:41:18 +0100

    Update Krokodoc

--- Playground/src/de/lubowiecki/workshop/dec15/App.java
+++ Playground/src/de/lubowiecki/workshop/dec15/App.java
@@ -3,6 +3,7 @@ package de.lubowiecki.workshop.dec15;
 public class App {
 
 	public static void main(String[] args) {
-		new Krokodoc().run();
+		//new Krokodoc().run();
+		new EnumKrokodoc().run();
 	}
 }
--- Playground/src/de/lubowiecki/workshop/dec15/Krokodoc.java
+++ Playground/src/de/lubowiecki/workshop/dec15/Krokodoc.java
@@ -1,25 +1,46 @@
 package de.lubowiecki.workshop.dec15;
 
+import java.util.Scanner;
+
 public class Krokodoc {
+	
+	private Krokodil krok = new Krokodil();
+	private Scanner scanner = new Scanner(System.in);
+	
 	
 	public void run() {
 		
-		Krokodil krok = new Krokodil();
+		int spieler = 1;
+		int runde = 1;
 		
-		System.out.println(krok.zeigeZaehne());
-		
-		int auswahl = 10;
-		
-		try {
-			krok.zieheZahn(auswahl);
-		} 
-		catch (KrankerZahnErwischtException e) {
-			System.out.println(e.getMessage());
-		} 
-		catch (ZahnBereitsGezogenException e) {
-			System.out.println(e.getMessage());
+		while(true) {
+			
+			System.out.println(krok.zeigeZaehne());
+			
+			System.out.println("Runde: " + runde);
+			System.out.println("Spieler: " + spieler);
+			
+			System.out.print("Auswahl: ");
+			
+			try {
+				krok.zieheZahn(scanner.nextInt());
+				System.out.println("Super. Zahn wurde gezogen.");
+				
+				runde++;
+				spieler = (runde % 2) == 0 ? 2 : 1;
+				
+			} 
+			catch (KrankerZahnErwischtException e) {
+				System.out.println("Aua!!!!!!!!");
+				break;
+				
+			} 
+			catch (ZahnBereitsGezogenException e) {
+				System.out.println("Zahn bereits gezogen!");
+				continue;
+			}
 		}
 		
-		System.out.println(krok.zeigeZaehne());
+		System.out.println("Spieler " + spieler + " hat verloren.");
 	}
 }
